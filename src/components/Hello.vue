4e21739bc030d596d7b23f8721caaea6b844c853
Brian <brian.elliot.toon@gmail.com>
2018-08-25 18:20:02 -0400

    initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="hello">
+    <b-container class="bv-example-row">
+      <b-row>
+          <b-col>1 of 3</b-col>
+          <b-col>2 of 3</b-col>
+          <b-col>3 of 3</b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
