d1b687f6f573ff48db16e04b176e0e00955890d2
Brian <brian.elliot.toon@gmail.com>
2018-09-25 19:17:53 -0400

    rental component added, getSliderPic method working to get images from array

--- src/components/Rental.vue
+++ src/components/Rental.vue
@@ -1,7 +1,19 @@
 <template>
     <div class="rental container" v-if="rental">
         <h2>{{ rental.title }}</h2>
+        <p>{{ rental.mainphoto }}</p>
         <b-img :src="getPicUrl(rental)" class="main-photo" />
+        
+        <!-- <p v-for="(image, index) in rental.sliderimages" :key="index"> {{ image }} </p> -->
+        
+        <div v-for="(image, index) in rental.sliderimages" :key="index">
+            <b-img :src="getSliderPic(rental, index)" class="main-photo" />
+        </div>
+
+        <p>{{ rental }}</p>
+
+        
+
     </div>
 </template>
 
@@ -18,16 +30,23 @@ export default {
     methods: {
         getPicUrl(rental){
             var images = require.context('../assets/', true, /\.jpg$/)
+            console.log(images('./' + rental.mainphoto))
             return images('./' + rental.mainphoto)
+        },
+        getSliderPic(rental, index){
+            var images = require.context('../assets/', true, /\.jpg$/)
+            console.log(images('./' + rental.sliderimages[index]))
+            return images('./' + rental.sliderimages[index])
         }
     },
     created(){
         let ref = db.collection('vaca-rentals').where('slug', '==', this.$route.params.rental_slug)
         ref.get().then(snapshot => {
             snapshot.forEach(doc => {
-                // console.log(doc.data())
+                console.log(doc.data())
                 this.rental = doc.data()
                 this.rental.id = doc.id
+                // console.log(this.rental)
             })
         })
     }
